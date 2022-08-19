b9eb6f443488761acb34b853ea656eb04670cd8a
Соловьев Роман <serrcel@gmail.com>
2022-08-19 13:43:49 +0400

    Added textbox and button. Add website have ICommand

---
+++ ViewModel/SettingsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_Destroyer.ViewModel
+{
+    internal class SettingsViewModel: ViewModelBase
+    {
+        public string SiteURL { get; set; }
+
+        public ICommand AddSiteUrl
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                   {
+                       Properties.Settings.Default.site_URL.Add(SiteURL);
+                       Properties.Settings.Default.Save();
+                   }
+                ) ;
+            }
+        }
+    }
+}
